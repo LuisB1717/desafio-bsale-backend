98c4150544b033ac8f84abe9955a5f1a85d70908
luisbarboz17 <luisbarboz17@gmail.com>
2022-10-13 17:59:40 -0500

    Connection established through a connection pool.

---
+++ src/config/mysql.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+async function doQuery(query, params) {
+  const result = await new Promise((resolve, reject) => {
+    pool.query(query, params, function (error, results) {
+      if (error) {
+        reject(error);
+        pool.connect();
+      } else {
+        resolve(results);
+      }
+    });
+  });
+
+  return result;
+}
+
+module.exports = { doQuery };
